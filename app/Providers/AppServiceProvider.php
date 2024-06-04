e86d2822c23854f08090b5a7ac294a8e8c67e9b7
Curtis Delicata <curtis.delicata@outlook.com>
2024-06-04 19:23:44 +0000

    Upgrade composer and npm packages

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Models\Page;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,7 +23,6 @@ class AppServiceProvider extends ServiceProvider
         //
     }
 }
-use App\Models\Page;
 
     /**
      * Bootstrap any application services.
