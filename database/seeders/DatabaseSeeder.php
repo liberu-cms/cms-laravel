314160fbab03db69b2874d956713899183a35461
raza9798 <m.raza9798@gmail.com>
2024-08-10 16:13:21 +0530

    Dynamic menu rendering upgrade

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,5 +19,9 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        $this->call([
+            MenuSeeder::class,
+        ]);
     }
 }
