91e277c96eb82aa2eab7be71562e68a9a3d5774f
raza9798 <m.raza9798@gmail.com>
2024-08-11 23:19:09 +0530

    contact resources created

---
+++ app/Livewire/Contact.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class Contact extends Component
+{
+    public function render()
+    {
+        return view('livewire.contact');
+    }
+}
