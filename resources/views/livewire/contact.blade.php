14b8d0d295384f360aecee4fa63cd36ae0180f51
raza9798 <m.raza9798@gmail.com>
2024-08-11 23:24:52 +0530

    contact ui update

--- resources/views/livewire/contact.blade.php
+++ resources/views/livewire/contact.blade.php
@@ -1,3 +1,105 @@
-<div>
-    {{-- Nothing in the world is as soft and yielding as water. --}}
-</div>
+<x-guest-layout>
+    <div class="">
+        <div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+            <div class="max-w-xl mx-auto">
+            <div class="text-center">
+                <h1 class="text-3xl font-bold text-gray-800 sm:text-4xl">
+                Contact us
+                </h1>
+                <p class="mt-1 text-gray-600">
+                We'd love to talk about how we can help you.
+                </p>
+            </div>
+            </div>
+        
+            <div class="mt-12 max-w-lg mx-auto">
+            <div class="flex flex-col border rounded-xl p-4 sm:p-6 lg:p-8">
+                <h2 class="mb-8 text-xl font-semibold text-gray-800">
+                Fill in the form
+                </h2>
+        
+                <form>
+                <div class="grid gap-4 lg:gap-6">
+                    <div class="grid grid-cols-1 sm:grid-cols-2 gap-4 lg:gap-6">
+                    <div>
+                        <label for="hs-firstname-contacts-1" class="block mb-2 text-sm text-gray-700 font-medium">First Name</label>
+                        <input type="text" name="hs-firstname-contacts-1" id="hs-firstname-contacts-1" class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none">
+                    </div>
+        
+                    <div>
+                        <label for="hs-lastname-contacts-1" class="block mb-2 text-sm text-gray-700 font-medium">Last Name</label>
+                        <input type="text" name="hs-lastname-contacts-1" id="hs-lastname-contacts-1" class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none">
+                    </div>
+                    </div>
+
+                    <div class="grid grid-cols-1 sm:grid-cols-2 gap-4 lg:gap-6">
+                    <div>
+                        <label for="hs-email-contacts-1" class="block mb-2 text-sm text-gray-700 font-medium">Email</label>
+                        <input type="email" name="hs-email-contacts-1" id="hs-email-contacts-1" autocomplete="email" class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none">
+                    </div>
+        
+                    <div>
+                        <label for="hs-phone-number-1" class="block mb-2 text-sm text-gray-700 font-medium">Phone Number</label>
+                        <input type="text" name="hs-phone-number-1" id="hs-phone-number-1" class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none">
+                    </div>
+                    </div>
+        
+                    <div>
+                    <label for="hs-about-contacts-1" class="block mb-2 text-sm text-gray-700 font-medium">Details</label>
+                    <textarea id="hs-about-contacts-1" name="hs-about-contacts-1" rows="4" class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none"></textarea>
+                    </div>
+                </div>
+        
+                <div class="mt-6 grid">
+                    <button type="submit" class="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none">Send inquiry</button>
+                </div>
+        
+                <div class="mt-3 text-center">
+                    <p class="text-sm text-gray-500">
+                    We'll get back to you in 1-2 business days.
+                    </p>
+                </div>
+                </form>
+            </div>
+            </div>
+        
+            <div class="mt-12 grid sm:grid-cols-2 lg:grid-cols-3 items-center gap-4 lg:gap-8">
+            <a class="group flex flex-col h-full text-center rounded-lg hover:bg-gray-100 focus:outline-none focus:bg-gray-100 p-4 sm:p-6" href="#">
+                <svg class="size-9 text-gray-800 mx-auto" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"/><path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3"/><path d="M12 17h.01"/></svg>
+                <div class="mt-5">
+                <h3 class="text-lg font-semibold text-gray-800">Knowledgebase</h3>
+                <p class="mt-1 text-gray-500">We're here to help with any questions or code.</p>
+                <p class="mt-5 inline-flex items-center gap-x-1 font-medium text-blue-600">
+                    Contact support
+                    <svg class="shrink-0 size-4 transition ease-in-out group-hover:translate-x-1 group-focus:translate-x-1" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m9 18 6-6-6-6"/></svg>
+                </p>
+                </div>
+            </a>
+        
+            <a class="group flex flex-col h-full text-center rounded-lg hover:bg-gray-100 focus:outline-none focus:bg-gray-100 p-4 sm:p-6" href="#">
+                <svg class="size-9 text-gray-800 mx-auto" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M14 9a2 2 0 0 1-2 2H6l-4 4V4c0-1.1.9-2 2-2h8a2 2 0 0 1 2 2v5Z"/><path d="M18 9h2a2 2 0 0 1 2 2v11l-4-4h-6a2 2 0 0 1-2-2v-1"/></svg>
+                <div class="mt-5">
+                <h3 class="text-lg font-semibold text-gray-800">FAQ</h3>
+                <p class="mt-1 text-gray-500">Search our FAQ for answers to anything you might ask.</p>
+                <p class="mt-5 inline-flex items-center gap-x-1 font-medium text-blue-600">
+                    Visit FAQ
+                    <svg class="shrink-0 size-4 transition ease-in-out group-hover:translate-x-1 group-focus:translate-x-1" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m9 18 6-6-6-6"/></svg>
+                </p>
+                </div>
+            </a>
+        
+            <a class="group flex flex-col h-full text-center rounded-lg hover:bg-gray-100 focus:outline-none focus:bg-gray-100 p-4 sm:p-6" href="#">
+                <svg class="size-9 text-gray-800 mx-auto" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m7 11 2-2-2-2"/><path d="M11 13h4"/><rect width="18" height="18" x="3" y="3" rx="2" ry="2"/></svg>
+                <div class="mt-5">
+                <h3 class="text-lg font-semibold text-gray-800">Developer APIs</h3>
+                <p class="mt-1 text-gray-500">Check out our development quickstart guide.</p>
+                <p class="mt-5 inline-flex items-center gap-x-1 font-medium text-blue-600">
+                    Contact sales
+                    <svg class="shrink-0 size-4 transition ease-in-out group-hover:translate-x-1 group-focus:translate-x-1" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m9 18 6-6-6-6"/></svg>
+                </p>
+                </div>
+            </a>
+            </div>
+        </div>
+    </div>
+</x-guest-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Livewire\Contact;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/contact', Contact::class);
 Route::get('/', \App\Http\Livewire\Homepage::class);
 
 Route::middleware([
