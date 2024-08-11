510a287e297fb93374e130f54ac9a6edccd99e55
raza9798 <m.raza9798@gmail.com>
2024-08-11 23:14:27 +0530

    about ui update

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Livewire\Homepage;
+use App\Livewire\About;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,7 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', \App\Http\Livewire\Homepage::class);
+Route::get('/', Homepage::class);
+Route::get('/about', About::class);
 
 Route::middleware([
     'auth:sanctum',
