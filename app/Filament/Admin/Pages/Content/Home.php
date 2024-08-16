11ab4f9353b68972963e1617e1e7041afaf691dc
raza9798 <m.raza9798@gmail.com>
2024-08-16 23:24:57 +0530

    dynamic home content phase 1

--- app/Filament/Admin/Pages/Content/Home.php
+++ app/Filament/Admin/Pages/Content/Home.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace App\Filament\Admin\Pages\Content;
-
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
 use Filament\Pages\Page;
 
 class Home extends Page
@@ -10,4 +12,15 @@ class Home extends Page
     protected static ?string $navigationIcon = 'heroicon-o-home';
     protected static ?string $navigationGroup = 'Editable Content';
     protected static string $view = 'filament.admin.pages.content.home';
+ 
+    public function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('title'),
+                TextColumn::make('slug'),
+                IconColumn::make('is_featured')
+                    ->boolean(),
+            ]);
+    }
 }
---
+++ app/Http/Controllers/HomeContentController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\HomeContent;
+use App\Http\Requests\StoreHomeContentRequest;
+use App\Http\Requests\UpdateHomeContentRequest;
+
+class HomeContentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreHomeContentRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(HomeContent $homeContent)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(HomeContent $homeContent)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateHomeContentRequest $request, HomeContent $homeContent)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(HomeContent $homeContent)
+    {
+        //
+    }
+}
