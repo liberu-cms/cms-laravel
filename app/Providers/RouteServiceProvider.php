5c4bd15d39a9f28b97299e9923651641afc04acf
raza9798 <m.raza9798@gmail.com>
2024-08-21 00:35:41 +0530

    unneccasry middleware removed

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -34,7 +34,7 @@ class RouteServiceProvider extends ServiceProvider
                 ->prefix('api')
                 ->group(base_path('routes/api.php'));
 
-            Route::middleware(['web', webModule::class])
+            Route::middleware('web')
                 ->group(base_path('routes/web.php'));
         });
     }
