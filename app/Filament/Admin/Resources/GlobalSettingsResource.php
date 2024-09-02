c4e0052dbcf651d49ea3c9c59ed4f01e6a874763
Raza9798 <m.raza9798@gmail.com>
2024-09-03 02:36:35 +0530

    liveweare components updated and crud fix

--- app/Filament/Admin/Resources/GlobalSettingsResource.php
+++ app/Filament/Admin/Resources/GlobalSettingsResource.php
@@ -3,11 +3,10 @@
 namespace App\Filament\Admin\Resources;
 
 use App\Filament\Admin\Resources\GlobalSettingsResource\Pages;
-use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
+use Filament\Forms;
 use Filament\Tables;
-use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Intelrx\Sitesettings\Models\SiteSettings;
 
@@ -21,40 +20,10 @@ class GlobalSettingsResource extends Resource
 
     public static function form(Form $form): Form
     {
-        // $table->string('name')->nullable();
-        //     $table->string('currency')->nullable();
-        //     $table->string('default_language')->nullable();
-        //     $table->text('address')->nullable();
-        //     $table->string('country')->nullable();
-        //     $table->string('email')->nullable();
-        //     $table->string('phone_01')->nullable();
-        //     $table->string('phone_02')->nullable();
-        //     $table->string('phone_03')->nullable();
-        //     $table->string('phone_04')->nullable();
-        //     $table->string('facebook')->nullable();
-        //     $table->string('twitter')->nullable();
-        //     $table->string('github')->nullable();
-        //     $table->string('youtube')->nullable();
-        //     $table->decimal('sales_commission_percentage', 5, 2)->default(1.00);
-        //     $table->decimal('lettings_commission_percentage', 5, 2)->default(8.00);
         return $form
             ->schema([
-                TextInput::make('name')->autocapitalize('words')->required(),
-                TextInput::make('currency')->autocapitalize('words')->required(),
-                TextInput::make('default_language')->autocapitalize('words')->required(),
-                TextInput::make('address')->autocapitalize('words')->required(),
-                TextInput::make('country')->autocapitalize('words')->required(),
-                TextInput::make('email')->autocapitalize('words')->required(),
-                TextInput::make('phone_01')->autocapitalize('words')->required(),
-                TextInput::make('phone_02')->autocapitalize('words'),
-                TextInput::make('phone_03')->autocapitalize('words'),
-                TextInput::make('phone_04')->autocapitalize('words'),
-                TextInput::make('facebook')->autocapitalize('words')->required(),
-                TextInput::make('twitter')->autocapitalize('words')->required(),
-                TextInput::make('github')->autocapitalize('words')->required(),
-                TextInput::make('youtube')->autocapitalize('words')->required(),
-                TextInput::make('sales_commission_percentage')->autocapitalize('words')->required(),
-                TextInput::make('lettings_commission_percentage')->autocapitalize('words')->required(),
+                Forms\Components\TextInput::make('key')->label('Title')->required(),
+                Forms\Components\TextInput::make('value')->required(),
             ]);
     }
 
@@ -62,12 +31,8 @@ class GlobalSettingsResource extends Resource
     {
         return $table
             ->columns([
-                TextColumn::make('name'),
-                TextColumn::make('currency'),
-                TextColumn::make('default_language')->label('language'),
-                TextColumn::make('country'),
-                TextColumn::make('email'),
-                TextColumn::make('phone_01')->label('Phone'),
+                Tables\Columns\TextColumn::make('key')->label('Title'),
+                Tables\Columns\TextColumn::make('value')->label('Value'),
             ])
             ->filters([
                 //
