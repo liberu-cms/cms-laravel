9911f8cdbbccf9fac87ad030bd074980de33a47b
Joshua Kisb <joshuakisb@gmail.com>
2024-09-06 08:30:40 +0300

    vite build admin theme

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
+                'resources/css/filament/admin/theme.css',
             ],
             refresh: true,
         }),
