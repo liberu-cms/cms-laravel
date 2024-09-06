d72830cf850f5e09ab3e6a9b460fdfd57927ccf6
Joshua Kisb <joshuakisb@gmail.com>
2024-09-06 08:04:00 +0300

    add user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,11 +2,8 @@
 
 namespace Database\Seeders;
 
-use App\Models\Team;
-use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,36 +12,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->withPersonalTeam()->create();
-
-
         $this->call([
             RolesSeeder::class,
             TeamSeeder::class,
-        ]);
-        $adminUser = User::create([
-            'name' => 'Admin User',
-            'email' => 'admin@example.com',
-            'password' => Hash::make('password'),
-            'email_verified_at' => now(),
-        ]);
-        $adminUser->assignRole('admin');
-        $this->createTeamForUser($adminUser);
-
-        $this->call([
+            UserSeeder::class,
             MenuSeeder::class,
             SiteSettingsSeeder::class,
             GuestLayoutManagmentSeeder::class,
         ]);
-
-        
-    }
-
-    private function createTeamForUser($user)
-    {
-        $team = Team::first();
-        $team->users()->attach($user);
-        $user->current_team_id = 1;
-        $user->save();
     }
 }
